744ac7f9f0d89c3f6f28144b43bc72500a46524d
Niloth-p <20315308+Niloth-p@users.noreply.github.com>
2023-10-05 14:30:59 +0530

    feat: Refactor model validators for scalability and prepare for logging

--- bug_hub/model_validators.py
+++ bug_hub/model_validators.py
@@ -6,24 +6,28 @@ from .models import Bug
 def validate_description_not_empty(value):
     """Ensure that the description field is not empty."""
     if not value.strip():
-        raise ValidationError("Description cannot be empty.")
+        error_message = "Description cannot be empty."
+        raise ValidationError(error_message)
 
 
 def validate_bug_type(value):
     """Ensure that the bug_type field value is one of the predefined choices"""
     choices = [choice[0] for choice in Bug.BUG_TYPE_CHOICES]
     if value not in choices:
-        raise ValidationError("Invalid bug type.")
+        error_message = "Invalid bug type."
+        raise ValidationError(error_message)
 
 
 def validate_report_date_not_future(value):
     """Ensure that the report_date field is in the past (no future reports allowed)."""
     if value > timezone.now():
-        raise ValidationError("Report date cannot be in the future.")
+        error_message = "Report date cannot be in the future."
+        raise ValidationError(error_message)
 
 
 def validate_status(value):
     """Ensure that the status field value is one of the predefined choices"""
     choices = [choice[0] for choice in Bug.STATUS_CHOICES]
     if value not in choices:
-        raise ValidationError("Invalid status.")
+        error_message = "Invalid status."
+        raise ValidationError(error_message)
