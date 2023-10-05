4f2b3086088039b1bee220e60c675a40ede32a6e
Niloth-p <20315308+Niloth-p@users.noreply.github.com>
2023-10-05 13:15:58 +0530

    docs(admin): Add docstrings

--- bug_hub/admin.py
+++ bug_hub/admin.py
@@ -1,8 +1,15 @@
+"""
+Django admin configuration for the bug_hub app
+"""
+
 from django.contrib import admin
 from .models import Bug
 
 
-# Register your models here.
 @admin.register(Bug)
 class BugAdmin(admin.ModelAdmin):
+    """
+    Django admin configuration for the Bug model
+    """
+
     list_display = ("description", "bug_type", "report_date", "status")
