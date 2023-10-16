e93ae5c1a6cdebb368642aeb77f84d4013564562
Niloth-p <20315308+Niloth-p@users.noreply.github.com>
2023-10-16 10:41:32 +0530

    feat: Make the dark theme button toggle between a moon-sun icon pair

--- static/common/js/dark_mode_script.js
+++ static/common/js/dark_mode_script.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
   const isDarkMode = localStorage.getItem("darkMode") === "true";
+  const themeIcon = $("#themeIcon");
 
   if (isDarkMode) {
+    themeIcon.removeClass("fa-moon");
+    themeIcon.addClass("fa-sun");
     $("body").addClass("dark-mode");
     $("table").addClass("table-dark");
   }
 
   function toggleDarkMode() {
+    themeIcon.toggleClass("fa-moon fa-sun");
+
     $("body").toggleClass("dark-mode");
     localStorage.setItem("darkMode", $("body").hasClass("dark-mode"));
 
