001e9de71f7d2cb0b681a7bea6a0c3434f4310e1
Niloth-p <20315308+Niloth-p@users.noreply.github.com>
2023-10-19 22:08:34 +0530

    feat(backup): add backup script
    Makes commit(s) on branch 'backup' and pushes to remote repo

---
+++ backup.sh
@@ -0,0 +1,25 @@
+# Source environment variables (PROJECT_DIR) from settings_local.py
+source GlitchGuru/settings_local.py
+
+SOURCE_BRANCH="main"
+BACKUP_BRANCH="backup"
+
+cd "$PROJECT_DIR"
+
+# Create or switch to the backup branch
+if ! git show-ref --verify --quiet "refs/heads/$BACKUP_BRANCH"; then
+    git checkout -b "$BACKUP_BRANCH"
+else
+    git checkout "$BACKUP_BRANCH"
+fi
+
+# Commit and push all changes in the project directory with a timestamped message
+git commit -m "Backup: $(date +'%Y-%m-%d %H:%M:%S')"
+if [[ -n $(git status -s) ]]; then
+    git add .
+    git commit -m "Backup: $(date +'%Y-%m-%d %H:%M:%S')"
+fi
+git push origin "$BACKUP_BRANCH"
+
+git checkout "$SOURCE_BRANCH"
+echo "Backup completed and pushed to branch '$BACKUP_BRANCH' with timestamp $TIMESTAMP."
