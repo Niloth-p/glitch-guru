d939fbf14f73b2651060f9b1216319995daf2b13
Niloth-p <20315308+Niloth-p@users.noreply.github.com>
2023-10-22 14:15:33 +0530

    feat: Add Sphinx extensions and configure settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,20 +14,33 @@ copyright = "2023, Niloth"
 author = "Niloth"
 release = "1.0.0"
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../.."))
 os.environ["DJANGO_SETTINGS_MODULE"] = "GlitchGuru.settings"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+                'sphinx.ext.autodoc',
+                'sphinx.ext.coverage',
+                'sphinx.ext.viewcode',
+                'sphinx.ext.todo',
+                'sphinx.ext.autosummary',
+                'sphinx.ext.autosectionlabel',
+                'sphinx_last_updated_by_git',
+                'notfound.extension',
+                'sphinx-prompt',
+                'sphinx_favicon',
+                'sphinx.ext.napoleon',
+              ]
 
 templates_path = ["_templates"]
 exclude_patterns = []
+html_favicon = '_static/Wmf-ico-48px.png'
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
