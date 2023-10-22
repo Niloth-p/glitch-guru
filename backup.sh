d55f519de408b2c647c4182594bf106ecc792501
Niloth-p <20315308+Niloth-p@users.noreply.github.com>
2023-10-22 18:28:10 +0530

    feat: Add progress indicators and fix call directory

--- backup.sh
+++ backup.sh
@@ -3,20 +3,21 @@ source .env
 SOURCE_BRANCH="main"
 BACKUP_BRANCH="backup"
 
-cd "$PROJECT_DIR"
-
 # Create or switch to the backup branch
 if ! git show-ref --verify --quiet "refs/heads/$BACKUP_BRANCH"; then
     git checkout -b "$BACKUP_BRANCH"
 else
     git checkout "$BACKUP_BRANCH"
 fi
+echo "Checked out the backup branch."
 
 # Commit and push all changes in the project directory with a timestamped message
 git commit -m "Backup: $(date +'%Y-%m-%d %H:%M:%S')"
+echo "Committed once"
 if [[ -n $(git status -s) ]]; then
     git add .
     git commit -m "Backup: $(date +'%Y-%m-%d %H:%M:%S')"
+    echo "Committed twice"
 fi
 git push origin "$BACKUP_BRANCH"
 
