c62756706940dea0e496ce64c9061a82d3072185
Niloth P <20315308+Niloth-p@users.noreply.github.com>
2023-11-25 15:48:25 +0530

    Update utils.py
    - Simplify the function get_choice_display()
    - Improve function docstring
    - Add module docstring

--- bug_hub/utils.py
+++ bug_hub/utils.py
@@ -1,6 +1,18 @@
-def get_choice_display(choices, key):
-    """Return the display name for a given key from a list of choices or 'All' if the key is not found."""
-    for code, display in choices:
-        if code == key:
-            return display
-    return 'All'
+"""
+Utility Functions
+
+This module contains utility functions that are not tied to specific Django models or views.
+These functions provide common functionality that can be reused across different parts of the project.
+"""
+def get_choice_display(choices, choice_key):
+    """
+    Get the display value for a choice key.
+
+    Args:
+        choices (list of tuple): The list of choices.
+        choice_key (str): The key of the choice.
+
+    Returns:
+        str: The display value for the choice.
+    """
+    return dict(choices).get(choice_key, "All")
